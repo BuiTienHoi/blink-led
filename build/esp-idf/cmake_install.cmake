dd28c84979ecb61267941a5ff2ae48bca18d5c9d
Hobui Cauut <cauuthobui17@gmail.com>
2026-08-02 00:17:22 +0300

    Complete course wifi

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -549,6 +549,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("D:/DEV_ESP32/Project_2/Project_Wifi/build/esp-idf/Project_wifi/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("D:/DEV_ESP32/Project_2/Project_Wifi/build/esp-idf/main/cmake_install.cmake")
 endif()
 
